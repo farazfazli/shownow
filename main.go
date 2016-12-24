de664cce46c2df751bd9c8a27a75ffa9020591fb
Faraz Fazli <farazfazli@gmail.com>
2016-12-24 12:08:40 -0500

    Update subdomain

--- main.go
+++ main.go
@@ -2,14 +2,14 @@
 package main
 
 import (
-	"fmt"
-	"os/exec"
 	"flag"
-	"strings"
+	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
+	"os/exec"
 	"strconv"
+	"strings"
 )
 
 /*
@@ -27,21 +27,21 @@ func checkError(msg string, err error) {
 func main() {
 	whoami := "faraz" // user.Current doesn't work when cross-compiling macOS -> Linux
 	userFlag := flag.String("user", "", "Username")
-	keyFlag :=  flag.String("key", "", "Public Key")
-	
+	keyFlag := flag.String("key", "", "Public Key")
+
 	flag.Parse()
-	
+
 	user := *userFlag
 	key := *keyFlag
-	
+
 	ip := getIP()
 	port := getFreePort()
-	
+
 	users, err := exec.Command("users").Output()
 	checkError("Error getting users", err)
-	
+
 	// Check if user exists
-	if (strings.Contains(user, "root") || strings.Contains(string(users), user)) {
+	if strings.Contains(user, "root") || strings.Contains(string(users), user) {
 		fmt.Println("User already exists")
 		fmt.Println("Flag passed: " + user)
 		fmt.Println(string(users))
@@ -49,25 +49,26 @@ func main() {
 	}
 
 	// Add user
-	_, err = exec.Command("/bin/sh", "-c", "sudo /usr/sbin/adduser --disabled-password --gecos \"\" " + user).Output()
+	_, err = exec.Command("/bin/sh", "-c", "sudo /usr/sbin/adduser --disabled-password --gecos \"\" "+user).Output()
 	checkError("Error creating user", err)
 	fmt.Printf("Added user: %s\n", user)
 
 	// Modify .ssh externally
-	_, err = exec.Command("/bin/sh", "-c", "cd /home/" + user + " && sudo mkdir .ssh && sudo chown -R " +user+":"+user+ " .ssh " + " && cd .ssh && touch authorized_keys && sudo chown " + whoami+":"+whoami + " authorized_keys").Output()
+	_, err = exec.Command("/bin/sh", "-c", "cd /home/"+user+" && sudo mkdir .ssh && sudo chown -R "+user+":"+user+" .ssh "+" && cd .ssh && touch authorized_keys && sudo chown "+whoami+":"+whoami+" authorized_keys").Output()
 	checkError("Error modifying .ssh", err)
 	fmt.Println("Created .ssh skeleton")
 
 	// Add SSH key & prevent user from running other commands
 	// Fix " right before key
-	reversecommand := `ssh `+user+`@`+ip+` -N -R `+port+`:localhost:`+port
-	command := `cd /home/`+user+`/.ssh/` + ` && sudo echo "command=\"SHELL=/bin/false && printf 'You cannot login. To tunnel, use the following:\n`+reversecommand+`\n'\",no-agent-forwarding,no-X11-forwarding,permitopen=\"localhost:`+port+`\" ` + key + `" > `+`authorized_keys && sudo chown `+ user + ":" + user + ` authorized_keys`
+	reversecommand := `ssh ` + user + `@` + ip + ` -N -R ` + port + `:localhost:` + port
+	command := `cd /home/` + user + `/.ssh/` + ` && sudo echo "command=\"SHELL=/bin/false && printf 'You cannot login. To tunnel, use the following:\n` + reversecommand + `\n'\",no-agent-forwarding,no-X11-forwarding,permitopen=\"localhost:` + port + `\" ` + key + `" > ` + `authorized_keys && sudo chown ` + user + ":" + user + ` authorized_keys`
 
 	_, err = exec.Command("/bin/sh", "-c", command).Output()
 	checkError("Error restricting .ssh to tunnel only", err)
 	fmt.Printf("Restricted %s tunneling ability for port %s only\n", user, port)
 
-	subdomain := "todo.faz.li"	// TODO NGINX config
+	tld = "ml"                            // ml or cf
+	subdomain := user + ".shownow." + tld // TODO NGINX config
 	fmt.Printf("alias shownow=%s && open %s\n", reversecommand, subdomain)
 }
 
@@ -91,7 +92,7 @@ func getFreePort() string {
 	defer l.Close()
 	free := strconv.Itoa(l.Addr().(*net.TCPAddr).Port)
 	fmt.Println("Port: " + free)
-	if (len(free) < 4) {
+	if len(free) < 4 {
 		panic("Error getting port")
 	}
 	return free
