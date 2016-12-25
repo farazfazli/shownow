5227954a3752a5aba2b1577850a63a7f95b914bc
Faraz Fazli <farazfazli@gmail.com>
2016-12-25 17:28:39 -0500

    Refactor strings

--- main.go
+++ main.go
@@ -22,6 +22,11 @@ func checkError(msg string, err error) {
 }
 
 func main() {
+	// Shell
+	sh := "/bin/sh"
+	// Comamnd flag
+	c := "-c"
+	
 	whoamiFlag := flag.String("whoami", "faraz", "Current Username") // user.Current doesn't work when cross-compiling macOS -> Linux
 	userFlag := flag.String("user", "", "Username")
 	keyFlag := flag.String("key", "", "Public Key")
@@ -55,35 +60,42 @@ func main() {
 	}
 
 	// Add user
-	_, err = exec.Command("/bin/sh", "-c", "sudo /usr/sbin/adduser --disabled-password --gecos \"\" "+user).Output()
+	addusrcmd := fmt.Sprintf(`sudo /usr/sbin/adduser --disabled-password --gecos "" %s`, user)
+	_, err = exec.Command(sh, c, addusrcmd).Output()
 	checkError("Error creating user", err)
 	fmt.Printf("Added user: %s\n", user)
 
 	// Modify .ssh externally
-	_, err = exec.Command("/bin/sh", "-c", "cd /home/"+user+" && sudo mkdir .ssh && sudo chown -R "+user+":"+user+" .ssh "+" && cd .ssh && touch authorized_keys && sudo chown "+whoami+":"+whoami+" authorized_keys").Output()
+	modsshcmd := fmt.Sprintf(`cd /home/%s && sudo mkdir .ssh && sudo chown -R %s:%s .ssh && cd .ssh && touch authorized_keys && sudo chown %s:%s authorized_keys`, user, user, user, whoami, whoami)
+	_, err = exec.Command(modsshcmd).Output()
 	checkError("Error modifying .ssh", err)
 	fmt.Println("Created .ssh skeleton")
 
 	// Add SSH key & prevent user from running other commands
-	reversecommand := `ssh ` + user + `@` + ip + ` -N -R ` + port + `:localhost:` + port
-	command := `cd /home/` + user + `/.ssh/` + ` && sudo echo "command=\"SHELL=/bin/false && printf 'You cannot login. To tunnel, use the following:\n` + reversecommand + `\n'\",no-agent-forwarding,no-X11-forwarding,permitopen=\"localhost:` + port + `\" ` + key + `" > ` + `authorized_keys && sudo chown ` + user + ":" + user + ` authorized_keys`
-
-	_, err = exec.Command("/bin/sh", "-c", command).Output()
+	reversecommand := fmt.Sprintf(`ssh %s@%s -N -R PORT:localhost:%s`, user, ip, port)
+	
+	restrictcmd := fmt.Sprintf(`cd /home/%s/.ssh && sudo echo "command=\"SHELL=/bin/false && printf 'You cannot login. To tunnel, use the following:\n%s and replace PORT with your local port.\n'\",no-agent-forwarding,no-X11-forwarding,permitopen=\"localhost:%s\" %s" > authorized_keys && sudo chown %s:%s authorized_keys`, user, reversecommand, port, key, user, user)
+	_, err = exec.Command(sh, c, restrictcmd).Output()
 	checkError("Error restricting .ssh to tunnel only", err)
-	fmt.Printf("Restricted %s tunneling ability for port %s only\n", user, port)
+	fmt.Printf("Restricted %s tunneling ability for port %s only\n\n", user, port)
 
 	subdomain := user + ".shownow." + tld
 
+	// Add NGINX configuration for reverse proxying subdomain
 	nginxConfig, err := os.Create("/etc/nginx/sites-enabled/"+user+".conf")
 	checkError("Error creating NGINX configuration", err)
-	nginxConfig.WriteString("server { listen 80; server_name "+user+".shownow.ml www."+user+".shownow.ml; location / { proxy_pass http://localhost:"+port+"; proxy_set_header Host $host; proxy_set_header X-Real-IP $remote_addr; proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; proxy_set_header X-Forwarded-Proto $scheme; } error_page 502 /502.html; location = /502.html { return 502 '"+tunnelOfflineMessage+"'; add_header Content-Type text/plain; } }")
+		nginxConf := fmt.Sprintf("server { listen 80; server_name %s.shownow.ml www.%s.shownow.ml; location / { proxy_pass http://localhost:%s; proxy_set_header Host $host; proxy_set_header X-Real-IP $remote_addr; proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; proxy_set_header X-Forwarded-Proto $scheme; } error_page 502 /502.html; location = /502.html { return 502 '%s'; add_header Content-Type text/plain; } }", user, user, port, tunnelOfflineMessage)
+	nginxConfig.WriteString(nginxConf)
 	nginxConfig.Sync()
 
-	err = exec.Command("/bin/sh", "-c", "sudo systemctl reload nginx").Start()
+	err = exec.Command(sh, c, "sudo systemctl reload nginx").Start()
 	checkError("Error restarting NGINX", err)
 
-	// TODO CloudFlare through API (instead of wildcard) for HTTPS
+	// Configured port alias - modify PORT & add to shell rc, then simply run: shownow
 	fmt.Printf("alias shownow=\"%s && open %s\"\n", reversecommand, subdomain)
+
+	// Function which takes a port number and sets up the tunnel - add to shell rc, and run like so: shownow 1000
+	fmt.Printf(`shownow() { ssh %s@%s -N -R $1\:localhost:%s; }`, user, ip, port)
 }
 
 // https://api.ipify.org - better than myexternalip
